96f5ad3f3b9d7163899395608c0575a511bc5e0b
Tom <56161932+pugzly@users.noreply.github.com>
2022-01-29 00:39:25 +0000

    Update bitquery_dex.py

--- bitquery_dex.py
+++ bitquery_dex.py
@@ -15,7 +15,7 @@ API_KEY = "YOUR___API___KEY___HERE"
 BASE_URL = "https://graphql.bitquery.io/"
 
 
-# change required token contract address and name:
+# change TOKEN contract address and name:
 token_name = "CAKE"
 token_list = {"TOKEN": "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
               "BUSD": "0xe9e7cea3dedca5984780bafc599bd69add087d56",
